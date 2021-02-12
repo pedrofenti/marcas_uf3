732f5c09b2119739a9f1ad5d67bbcc5670f3f4bb
pedrofenti <pedro.fernandez@enti.cat>
2021-02-12 14:34:09 +0100

    Mejorando el juegazo

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 const http = require("http");
 const fs = require("fs");
+const node_static = require("node-static");
 
 console.log("Inicializando el servidor web");
+
+let public_files = new(node_static.Server)("/home/enti/marcas_uf3/pub");
 
 http.createServer((request, response) => {
 	console.log(request.url);
 
+	public_files. serve(request, response);
+
+
+
+/*
 	if (request.url == "/fondo_estrellado.jpg"){
 		fs.readFile("fondo_estrellado.jpg", (error, content) => {
 			response.writeHead(200, {'Content-Type': 'image/jpg'});
@@ -25,6 +33,6 @@ http.createServer((request, response) => {
 			response.end();
 		});
 	}
-
+*/
 }).listen(8080);
 
